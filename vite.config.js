d83d729f5b9591afdfc174b0199c2c100434df0b
BayanMuaddel <bayanmouddil@gmail.com>
2026-07-07 23:06:23 +0300

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Digital-Clock-Vue/',
   plugins: [
     vue(),
     vueDevTools(),
